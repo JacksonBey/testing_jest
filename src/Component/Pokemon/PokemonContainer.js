4265dc40a6ff39589ac1bca3abd33b9b70ed2977
JacksonBey <jacksongrowson@gmail.com>
2021-11-01 14:44:02 -0700

    basic error display

--- src/Component/Pokemon/PokemonContainer.js
+++ src/Component/Pokemon/PokemonContainer.js
@@ -6,31 +6,49 @@ const PokemonContainer = () => {
   const [error,setError] = useState(null)
   const [pokemonQuery,setPokemonQuery] = useState('')
   const [pokemonImageURL,setPokemonImageURL] = useState('')
-
+  const [errorMessage,setErrorMessage] = useState('')
 
 
   const fetchData = async () => {
     setLoading(true)
     // sanatize the query
     const sanatizedQuery = pokemonQuery.toLowerCase().trim()
-    try {
       fetch(`https://pokeapi.co/api/v2/pokemon/${sanatizedQuery}`)
-      .then(res => res.json())
+      .then(res => {
+        if(res.status === 200) {
+          return res.json()
+        }else {
+          setError(true)
+          setErrorMessage(`${sanatizedQuery} is not a valid pokemon name`)
+        }
+      })
       .then(data => {
         console.log('DATA: ', data)
-        setPokemon(data)
-        setLoading(false)
-        setPokemonImageURL(`${data.sprites.front_default}`)
+        if(!data){
+          setError(true)
+          setErrorMessage('Pokemon not found')
+        } else {
+          setPokemon(data)
+          setLoading(false)
+          setPokemonImageURL(`${data?.sprites?.front_default}`)
+          setError(false)
+          setErrorMessage('')
+        }
+
       }
       )
       //finds the image url from google images
       // const imageURL = await fetch(`https://www.google.com/search?q=${sanatizedQuery}+pokemon&tbm=isch&tbs=isz:m`)
-
-    } catch (error) {
-      console.log('ERROR: ', error)
-      setError(error)
-    }
+      // if(fetchError){
+      //   console.log('INFECTCH EROOR!')
+      //   setError(true)
+      //   setErrorMessage('Pokemon not found')
+      // }
+      setLoading(false)
   }
+
+
+
 
   useEffect(() => {
 
@@ -44,8 +62,8 @@ const PokemonContainer = () => {
       <label for="pokemonQuery">Search for a pokemon </label>
     <input type="text" id="pokemonQuery" value={pokemonQuery} onChange={(e) => setPokemonQuery(e.target.value)}/>
     <button onClick={() => fetchData()}> Search</button>
-      {loading ? 'Loading...' : pokemon.name}
-      {error ? error.message : null}
+      {loading ? 'Loading...' : pokemon?.name}
+      {error ? errorMessage : null}
       <button onClick={() => console.log('POKEMON: ', pokemon)}>pokemon?</button>
       <img src={pokemonImageURL}/>
     </div>
